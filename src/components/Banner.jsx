558d922e53e569f998a3316b4cf330d423df3617
Emmanuel Phanuel <67367395+Itzemmanuel@users.noreply.github.com>
2024-08-23 14:22:43 +0200

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ export default function Banner() {
             <div className='flex flex-col lg:flex-row'>
                 <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
                     <h1 className='tracking-wider text-4xl lg:text-[58px] font-bold leading-none mb-6'><span className='text-cyan-500 transition animate-pulse'>Find</span> Your Dream House With Us.</h1>
-                    <p className='font-medium max-w-[480px] mb-8'>
+                    <p className='font-medium max-w-[480px] mb-8 text-slate-500'>
                         Join our community of renters who have found their perfect homes with myRealtor. Start your search today and unlock a world of possibilities. Your dream rental home is just a few clicks away!
                     </p>
                 </div>
