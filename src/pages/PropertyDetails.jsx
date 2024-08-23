0d4169e3eb803dfe7404eaf9c44dba5886444f65
Emmanuel Phanuel <67367395+Itzemmanuel@users.noreply.github.com>
2024-08-23 14:20:20 +0200

    Update PropertyDetails.jsx

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -15,22 +15,22 @@ export default function PropertyDetails() {
 
     return(
         <section>
-            <div className='container mx-auto min-h-[800px] mb-14'>
+            <div className='container mx-auto px-8 md:px-0 min-h-[800px] mb-14'>
                 <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between'>
                     <div>
                         <h2 className='text-2xl font-semibold'>
                            {house.name} 
                         </h2>
-                        <h3 className='text-lg mb-4'>
+                        <h3 className='mb-4 text-lg'>
                            {house.address} 
                         </h3>
                     </div>
-                    <div className='mb-4 mr-14 lg:mb-0 flex gap-x-2 text-sm'>
-                        <div className='bg-green-500 text-white px-3 rounded-full'>
+                    <div className='flex mb-4 text-sm mr-14 lg:mb-0 gap-x-2'>
+                        <div className='px-3 text-white bg-green-500 rounded-full'>
                             {house.type}
                               
                         </div>
-                        <div className='bg-cyan-500 text-white px-3 rounded-full'>
+                        <div className='px-3 text-white rounded-full bg-cyan-500'>
                             {house.country}
                         </div>
                     </div>
@@ -43,20 +43,20 @@ export default function PropertyDetails() {
                         <div className='mb-8'>
                             <img src={house.imageLg} alt="House Image" />
                         </div>
-                        <div className='flex gap-x-6 text-cyan-600 mb-6'>
-                            <div className='flex gap-x-2 items-center'>
+                        <div className='flex mb-6 gap-x-6 text-cyan-600'>
+                            <div className='flex items-center gap-x-2'>
                                 <BiBed className='text-2xl' />
                                 <div>
                                     {house.bedrooms} 
                                 </div>
                             </div>
-                            <div className='flex gap-x-2 items-center'>
+                            <div className='flex items-center gap-x-2'>
                                 <BiBath className='text-2xl' />
                                 <div>
                                     {house.bathrooms}
                                 </div>
                             </div>
-                            <div className='flex gap-x-2 items-center'>
+                            <div className='flex items-center gap-x-2'>
                                 <BiArea className='text-2xl'  />
                                 <div>
                                     {house.surface} 
@@ -67,36 +67,36 @@ export default function PropertyDetails() {
                             {house.description}
                         </div>
                     </div>
-                    <div className='flex-1 bg-white w-full mb-8 border border-gray-300 rounded-lg px-6 py-8 '>
-                        <div className='flex items-center gap-x-4 mb-8'>
+                    <div className='flex-1 w-full px-6 py-8 mb-8 bg-white border border-gray-300 rounded-lg '>
+                        <div className='flex items-center mb-8 gap-x-4'>
                             <div className='w-20 h-20 p-1 border border-gray-300 rounded-full'>
                                <img src={house.agent.image} alt="Realtor agent" /> 
                             </div>
                             <div>
-                                <div className='font-bold text-lg'>
+                                <div className='text-lg font-bold'>
                                     {house.agent.name}
                                 </div>
-                                <Link to='' className='text-cyan-600 hover:text-cyan-500 font-medium text-sm'>
+                                <Link to='' className='text-sm font-medium text-cyan-600 hover:text-cyan-500'>
                                     View Listings
                                 </Link>
                             </div>
                         </div>
                         <form className='flex flex-col gap-y-4'>
-                            <input className='border border-gray-300 focus:border-cyan-500 outline-none rounded w-full px-4 h-14 text-sm text-slate-950'  type="text"
+                            <input className='w-full px-4 text-sm border border-gray-300 rounded outline-none focus:border-cyan-500 h-14 text-slate-950'  type="text"
                             placeholder='Name' />
-                            <input className='border border-gray-300 focus:border-cyan-500 outline-none rounded w-full px-4 h-14 text-sm text-slate-950'  type="tel"
+                            <input className='w-full px-4 text-sm border border-gray-300 rounded outline-none focus:border-cyan-500 h-14 text-slate-950'  type="tel"
                             placeholder='Phone Number' />
-                             <input className='border border-gray-300 focus:border-cyan-500 outline-none rounded w-full px-4 h-14 text-sm text-slate-950'
+                             <input className='w-full px-4 text-sm border border-gray-300 rounded outline-none focus:border-cyan-500 h-14 text-slate-950'
                              type="email"
                              placeholder='Email' />
-                            <textarea className='border border-gray-300 focus:border-cyan-500 outline-none resize-none rounded w-full p-4 h-36 text-sm text-slate-950'
+                            <textarea className='w-full p-4 text-sm border border-gray-300 rounded outline-none resize-none focus:border-cyan-500 h-36 text-slate-950'
                             placeholder='Message'>
                             </textarea>
                             <div className='flex gap-x-2'>
-                                <button className='bg-cyan-800 hover:bg-cyan-600 text-white rounded p-4 text-sm w-full transition'>
+                                <button className='w-full p-4 text-sm text-white transition rounded bg-cyan-800 hover:bg-cyan-600'>
                                     Send message
                                 </button>
-                                <button className=' border border-cyan-700 text-cyan-700 hover:border-cyan-500 hover:text-cyan-500 rounded p-4 text-sm w-full transition'>
+                                <button className='w-full p-4 text-sm transition border rounded border-cyan-700 text-cyan-700 hover:border-cyan-500 hover:text-cyan-500'>
                                     Call
                                 </button>
                             </div>
